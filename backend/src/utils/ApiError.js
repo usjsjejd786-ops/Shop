bf8b1ddaaef0620f1d4683cd62938dd191f83556
usjsjejd786-ops <usjsjejd786@gmail.com>
2026-09-17 20:22:05 -0300

    Add files via upload

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,9 @@
+// Erro padronizado para respostas HTTP consistentes.
+class ApiError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = ApiError;
